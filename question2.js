6acf859aefd66e491193e1e6f4a5a4f9b750191f
Chandana-D28 <152282340+Chandana-D28@users.noreply.github.com>
2025-04-21 18:47:06 +0530

    Delete question2.js

--- question2.js
+++
@@ -1,7 +0,0 @@
-function flatAndSort(arr){
-    let flatArr=arr.flat(Infinity)
-    let sortArr=flatArr.sort((a,b)=>a-b);
-    console.log(sortArr);
-
-}
-flatAndSort([[3, 2, 1], [4, 5, 2], [1, 6]]);
