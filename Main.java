ed495cf5c197861c31c66405aebc5cb550179619
Chandana-D28 <chandana2812004@gmail.com>
2025-04-30 14:22:13 +0530

    custom array added

---
+++ Main.java
@@ -0,0 +1,21 @@
+package com.dyashin.assignment.arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		MyArray myArray=new MyArray();
+		myArray.add("Apple");
+		myArray.add("Banana");
+		myArray.add("Cherry");
+		myArray.add("Dragon fruit");
+		myArray.add("Fruits");
+		myArray.display();
+		
+		System.out.println(myArray.getString(1));
+	
+		
+		
+
+	}
+
+}
